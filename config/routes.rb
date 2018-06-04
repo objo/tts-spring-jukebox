645cf44b295b09d5df984c88d94f52e5661027b3
Joe OBrien <joe@objo.com>
2018-06-04 17:49:28 -0400

    added artist CRUD into the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :artists
   root 'welcome#index'
 
   get '/hello' => 'welcome#hello'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180530221550) do
+ActiveRecord::Schema.define(version: 20180604214125) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
